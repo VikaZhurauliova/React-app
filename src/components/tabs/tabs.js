2b335c3b4a33e6bdba282cf8796b6432972e46dd
vika zhurauliova <vika.zhurauliova.19@gmail.com>
2023-06-23 22:38:44 +0300

    fix

--- src/components/tabs/tabs.js
+++ src/components/tabs/tabs.js
@@ -1,30 +1,17 @@
 import './tabs.scss'
 
-const Tabs = () => {
-    const activeTab = () => {
-        let tabs = document.getElementsByClassName("tabs-wrap-cell");
-        for (let i = 0; i < tabs.length; i++) {
-            tabs[i].addEventListener("click", function() {
-                let current = document.getElementsByClassName("tabs-active");
-                current[0].className = current[0].className.replace(" tabs-active", "");
-                this.className += " tabs-active";
-            });
-        }
-    }
+const Tabs = ({ tabs = [], activeTab, setActiveTab }) => {
+    const isTabActive = (name) => activeTab === name;
 
     return(
         <div className="tabs">
             <p className="tabs-title">Tabs</p>
             <div className="tabs-wrap">
-                <div className="tabs-wrap-cell tabs-active">
-                    <p className="tabs-wrap-cell-title" onClick={activeTab}>All</p>
-                </div>
-                <div className="tabs-wrap-cell">
-                    <p className="tabs-wrap-cell-title" onClick={activeTab}>My favorites</p>
-                </div>
-                <div className="tabs-wrap-cell">
-                    <p className="tabs-wrap-cell-title" onClick={activeTab}>Popular</p>
-                </div>
+                { tabs.map(({ title, key }) => (
+                    <div key={key} className={`tabs-wrap-cell ${isTabActive(key) ? 'tabs-active' : ''}`}>
+                        <p className="tabs-wrap-cell-title" onClick={() => setActiveTab(key)}>{title}</p>
+                    </div>
+                )) }
             </div>
         </div>
     )
