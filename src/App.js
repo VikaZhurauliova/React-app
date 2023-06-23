4d616ceaad35064f6e8cf120ee4fa385ffb028a6
vika zhurauliova <vika.zhurauliova.19@gmail.com>
2023-06-23 23:19:09 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
+import {useState} from "react";
+
 import Menu from "./components/menu/menu";
 import Tabs from "./components/tabs/tabs";
+import { TABS } from "./constants/tabs";
+import Blog from "./components/blog/blog";
+
 import './App.scss';
 
 function App() {
-  const user = {
+    const [ activeTab, setActiveTab ] = useState(TABS[0]?.key);
+    const user = {
     firstName: "Nick",
     lastName: "Denskiy"
-  }
+    }
   return(
       <div className="App">
         <Menu user={user}/>
-        <Tabs />
+          <div className="container">
+              <Tabs activeTab={activeTab} setActiveTab={setActiveTab} tabs={TABS} />
+              <Blog/>
+          </div>
+
       </div>
   )
 }
